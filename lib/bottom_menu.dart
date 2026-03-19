fb1ec2877118716bdfea39579654b36c3171ecb0
Sohib <abdumanobovsohib13@gmail.com>
2026-03-19 14:33:49 +0500

    Added bottom navigation bar. The rest will be about implementing the screens themselves.

---
+++ lib/bottom_menu.dart
@@ -0,0 +1,57 @@
+import 'package:clip_react/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BottomMenu extends StatelessWidget {
+  const BottomMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        navigationBarTheme: NavigationBarThemeData(
+          indicatorColor: Colors.transparent,
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+          iconTheme: WidgetStateProperty.resolveWith((states) {
+            if (states.contains(WidgetState.selected)) {
+              return const IconThemeData(color: Colors.black, size: 28);
+            }
+            return const IconThemeData(color: Colors.grey, size: 28);
+          }),
+        ),
+      ),
+      home: Scaffold(
+        bottomNavigationBar: Obx(
+          () => NavigationBar(
+            height: 60,
+            elevation: 0,
+            backgroundColor: Colors.white,
+            indicatorColor: Colors.transparent,
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (index) =>
+                controller.selectedIndex.value = index,
+            destinations: [
+              NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+              NavigationDestination(icon: Icon(Icons.movie), label: 'Clips'),
+              NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+            ],
+          ),
+        ),
+        body: Obx(() => controller.screens[controller.selectedIndex.value]),
+      ),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+  final screens = [
+    HomeScreen(),
+    Container(color: Colors.blue),
+    Container(color: Colors.yellow),
+  ];
+}
