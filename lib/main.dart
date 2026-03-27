6be6eca601d430eaee2526eb4801a119b8568b40
Sohib <abdumanobovsohib13@gmail.com>
2026-03-27 11:18:41 +0500

    Added authentication, so users can create their account and save their record

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:clip_react/bottom_menu.dart';
+import 'package:clip_react/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const BottomMenu());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp(FirebaseUserRepo()));
 }
